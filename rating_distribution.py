ba160335f4321a033b50cb5e97da88c01cae9fc3
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2024-09-12 11:22:52 +0200

    Normalize the data

--- rating_distribution.py
+++ rating_distribution.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 # Read the CSV file and save it to X and Y
@@ -9,16 +10,15 @@ Y = []
 directory_path = Path('data/')
 
 
-for file in directory_path.rglob('*.csv'):  
+for i, file in enumerate(directory_path.rglob('*.csv')):  
     data = pd.read_csv(file)
     
     Y.extend(data.Rating)
+    print(f"For tester {i+1}, he/she use a range from {max(data.Rating)} to {min(data.Rating)} for rating")
     # Convert list of flattened images to a numpy array
 
 labels = np.array(Y)
 
-# %%
-labels
 # %%
 # Plot the distribution of labels using a histogram
 plt.figure(figsize=(8, 6))
@@ -27,9 +27,23 @@ plt.title('Distribution of Ratings')
 plt.xlabel('Rating')
 plt.ylabel('Frequency')
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.savefig("distribution.jpg")
 plt.show()
 
 
-
 # %%
-Y
+# Initialize MinMaxScaler to scale data to range 0-1
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+# Reshape labels if it's a 1D array (scikit-learn expects 2D input)
+labels = labels.reshape(-1, 1)
+
+# Fit the scaler to your data and transform it
+normalized_labels = scaler.fit_transform(labels)
+
+# If you want to flatten it back to 1D (in case of a single feature):
+normalized_labels = normalized_labels.flatten()
+
+# Print normalized labels
+print("Normalized Labels:", normalized_labels)
+# %%
